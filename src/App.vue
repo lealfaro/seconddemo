6d4b02c1eaa7a821831fed203d43819514de9605
lealfaro <115046310+lealfaro@users.noreply.github.com>
2023-03-29 13:52:39 -0600

    user

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,45 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/contacto">Contacto</router-link> |
-  
+    <a v-if="!userInfo" :href="`/.auth/login/github?post_login_redirect_uri=${redirect}`"> Login </a>
+    <template v-if="userInfo">
+        <a v-if="userInfo" :href="`/.auth/logout`"> Logout </a> |
+        {{ userInfo.userDetails }} 
+    </template>
   </nav>
   <router-view/>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            userInfo: {
+                type: Object,
+                default() { },
+            },
+            redirect: window.location.pathname,
+        };
+    },
+    methods: {
+        async getUserInfo() {
+            try {
+                const response = await fetch("/.auth/me");
+                const payload = await response.json();
+                const { clientPrincipal } = payload;
+                return clientPrincipal;
+            }
+            catch (error) {
+                console.error("No profile could be found");
+                return undefined;
+            }
+        },
+    },
+    async created() {
+        this.userInfo = await this.getUserInfo();
+    }
+};
+</script>
+
 
 <style>
 #app {
