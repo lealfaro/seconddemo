7de8618d9e55b76914a85726088096b5848f5887
lealfaro <115046310+lealfaro@users.noreply.github.com>
2023-03-29 13:42:14 -0600

    call api

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="about">
       <h1>Gracias por contactarnos</h1>
+      {{message}}
     </div>
   </template>
+
+  <script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        message: ""
+      };
+    },
+    async mounted() {
+      const { text } = await (await fetch("/api/mtc")).json();
+      this.message = text;
+    }
+  };
+  </script>
   
